ed006d0c675a106d9ede089e90560323f20bf3e4
Arnaud BRETON <arnaud@unishared.com>
2013-04-12 10:01:57 -0700

    Add Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: '<json:package.json>',
+        watch: {
+            karma: {
+                files: ['angular-youtube-player-api.js', 'test/unit/*.js'],
+                tasks: ['karma:unitBackground:run'] //NOTE the :run flag
+            }
+        },
+        karma: {
+            options: {
+                configFile: 'config/karma.conf.js',
+                browsers: ['Chrome', 'Firefox']
+            },
+
+            unit: {
+                singleRun: true
+            },
+
+            unitBackground: {
+                background: true
+            }
+        },
+        uglify: {
+            build: {
+                files: {
+                    'build/angular-youtube-player-api.min.js': [
+                        'angular-youtube-player-api.js'
+                    ]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-karma');
+
+    // Default task.
+    grunt.registerTask('default', ['uglify']);
+};
