efc1b09d846e445181dee410ee0bd565d408e89e
brandly <brandly13@gmail.com>
2014-05-19 21:38:30 -0400

    remove a big comment about protocol relative URLs

--- angular-youtube-player-api.js
+++ angular-youtube-player-api.js
@@ -1,10 +1,5 @@
 angular.module('youtube', ['ng']).run(function () {
     var tag = document.createElement('script');
-
-    // This is a protocol-relative URL as described here:
-    //     http://paulirish.com/2010/the-protocol-relative-url/
-    // If you're testing a local page accessed via a file:/// URL, please set tag.src to
-    //     "https://www.youtube.com/iframe_api" instead.
     tag.src = "//www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
