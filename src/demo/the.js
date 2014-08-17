4e700f45265ff2ea550217d74db3d169657f8c50
brandly <brandly13@gmail.com>
2014-08-17 19:52:18 -0400

    update demo with a route change

---
+++ src/demo/the.js
@@ -0,0 +1,37 @@
+// TODO: write a small app with a couple routes, to display
+//    * routing capabilities
+//    * video id
+//    * video url
+//    * various player options
+
+// Create your app with 'youtube-embed' dependency
+var myApp = angular.module('myApp', ['youtube-embed', 'ngRoute']);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+   .when('/', {
+    templateUrl: 'demo/the.html',
+    controller: 'TheCtrl'
+  })
+  .when('/advanced', {
+    templateUrl: 'demo/advanced.html',
+    controller: 'AdvancedCtrl'
+  })
+  .otherwise('/');
+}]);
+
+// Inside your controller...
+myApp.controller('TheCtrl', function ($scope) {
+    // have a video ID
+    $scope.theBestVideo = 'sMKoNBRZM1M';
+
+    // or a URL
+    $scope.anotherGoodOne = 'https://www.youtube.com/watch?v=18-xvIjH8T4';
+});
+
+myApp.controller('AdvancedCtrl', function ($scope) {
+    $scope.selfPlayingVideo = 'sMKoNBRZM1M';
+    $scope.selfPlayingVars = {
+        autoplay: 1
+    };
+});
